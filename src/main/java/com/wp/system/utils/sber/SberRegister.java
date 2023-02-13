83e92ea6ff34ce162c542e37ad98a39a0d4c559d
Yesbolat Syilybay <beskaska@gmail.com>
2023-02-13 14:49:45 +0500

    turn on certificate validating

--- src/main/java/com/wp/system/utils/sber/SberRegister.java
+++ src/main/java/com/wp/system/utils/sber/SberRegister.java
@@ -10,21 +10,12 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import javax.net.ssl.SSLContext;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-
 public class SberRegister {
     private UUID userId;
 
     private String phone;
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     private String jSession;
 
@@ -43,17 +34,6 @@ public class SberRegister {
     public SberRegister(UUID userId, String phone) {
         this.userId = userId;
         this.phone = phone;
-    }
-
-    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
-        return restTemplate;
     }
 
     public void register() {
@@ -81,7 +61,7 @@ public class SberRegister {
 
             HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
-            ResponseEntity<String> response = restTemplate().postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", request , String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", request , String.class);
 
             Integer responseCode = SberUtils.getCodeFromResponse(response.getBody());
 
@@ -137,7 +117,7 @@ public class SberRegister {
 
             HttpEntity<MultiValueMap<String, String>> confirmRequest = new HttpEntity<MultiValueMap<String, String>>(confirmRequestBody, headers);
 
-            ResponseEntity<String> confirmResponse = restTemplate().postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", confirmRequest , String.class);
+            ResponseEntity<String> confirmResponse = restTemplate.postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", confirmRequest , String.class);
 
             System.out.println(confirmResponse.getBody());
 
@@ -154,56 +134,52 @@ public class SberRegister {
     }
 
     public void createPin() {
-        try {
-            if(!state.equals(SberRegisterState.WAIT_CREATE_PIN))
-                throw new ServiceException("Try recreate sber register instance", HttpStatus.INTERNAL_SERVER_ERROR);
+        if(!state.equals(SberRegisterState.WAIT_CREATE_PIN))
+            throw new ServiceException("Try recreate sber register instance", HttpStatus.INTERNAL_SERVER_ERROR);
 
-            state = SberRegisterState.CREATE_PIN;
+        state = SberRegisterState.CREATE_PIN;
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            headers.set("Set-Cookie", jSession);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Set-Cookie", jSession);
 
-            MultiValueMap<String, String> createPinRequestBody = new LinkedMultiValueMap<String, String>();
-            createPinRequestBody.add("operation", "createPIN");
-            createPinRequestBody.add("password", "17856");
-            createPinRequestBody.add("mGUID", mGUID);
-            createPinRequestBody.add("appType", "android");
-            createPinRequestBody.add("version", "9.20");
-            createPinRequestBody.add("appVersion", "12.15.0");
-            createPinRequestBody.add("deviceName", "HUAWEI_ANE-LX1");
-            createPinRequestBody.add("devID", "607d725604d1f032e50bb3c0622e791d3f400000");
-            createPinRequestBody.add("devIdOld", "607d725604d1f032e50bb3c0622e791d3f400000");
+        MultiValueMap<String, String> createPinRequestBody = new LinkedMultiValueMap<String, String>();
+        createPinRequestBody.add("operation", "createPIN");
+        createPinRequestBody.add("password", "17856");
+        createPinRequestBody.add("mGUID", mGUID);
+        createPinRequestBody.add("appType", "android");
+        createPinRequestBody.add("version", "9.20");
+        createPinRequestBody.add("appVersion", "12.15.0");
+        createPinRequestBody.add("deviceName", "HUAWEI_ANE-LX1");
+        createPinRequestBody.add("devID", "607d725604d1f032e50bb3c0622e791d3f400000");
+        createPinRequestBody.add("devIdOld", "607d725604d1f032e50bb3c0622e791d3f400000");
 
-            createPinRequestBody.add("mobileSdkData", "{\\\"TIMESTAMP\\\":\\\"2019-09-13T07:23:14Z\\\",\\\"HardwareID\\\":\\\"-1\\\",\\\"SIM_ID\\\":\\\"-1\\\",\\\"PhoneNumber\\\":\\\"-1\\\",\\\"GeoLocationInfo\\\":[{\\\"Timestamp\\\":\\\"0\\\",\\\"Status\\\":\\\"1\\\"}],\\\"DeviceModel\\\":\\\"ANE-LX1\\\",\\\"MultitaskingSupported\\\":true,\\\"DeviceName\\\":\\\"marky\\\",\\\"DeviceSystemName\\\":\\\"Android\\\",\\\"DeviceSystemVersion\\\":\\\"28\\\",\\\"Languages\\\":\\\"ru\\\",\\\"WiFiMacAddress\\\":\\\"02:00:00:00:00:00\\\",\\\"WiFiNetworksData\\\":{\\\"BBSID\\\":\\\"02:00:00:00:00:00\\\",\\\"SignalStrength\\\":\\\"-47\\\",\\\"Channel\\\":\\\"null\\\"},\\\"CellTowerId\\\":\\\"-1\\\",\\\"LocationAreaCode\\\":\\\"-1\\\",\\\"ScreenSize\\\":\\\"1080x2060\\\",\\\"RSA_ApplicationKey\\\":\\\"2C501591EA5BF79F1C0ABA8B628C2571\\\",\\\"MCC\\\":\\\"286\\\",\\\"MNC\\\":\\\"02\\\",\\\"OS_ID\\\":\\\"1f32651b72df5515\\\",\\\"SDK_VERSION\\\":\\\"3.10.0\\\",\\\"Compromised\\\":0,\\\"Emulator\\\":0}");
-            createPinRequestBody.add("mobileSDKKAV", "{\\\"osVersion\\\":0,\\\"KavSdkId\\\":\\\"\\\",\\\"KavSdkVersion\\\":\\\"\\\",\\\"KavSdkVirusDBVersion\\\":\\\"SdkVirusDbInfo(year=0, month=0, day=0, hour=0, minute=0, second=0, knownThreatsCount=0, records=0, size=0)\\\",\\\"KavSdkVirusDBStatus\\\":\\\"\\\",\\\"KavSdkVirusDBStatusDate\\\":\\\"\\\",\\\"KavSdkRoot\\\":false,\\\"LowPasswordQuality\\\":false,\\\"NonMarketAppsAllowed\\\":false,\\\"UsbDebugOn\\\":false,\\\"ScanStatus\\\":\\\"NONE\\\"}");
+        createPinRequestBody.add("mobileSdkData", "{\\\"TIMESTAMP\\\":\\\"2019-09-13T07:23:14Z\\\",\\\"HardwareID\\\":\\\"-1\\\",\\\"SIM_ID\\\":\\\"-1\\\",\\\"PhoneNumber\\\":\\\"-1\\\",\\\"GeoLocationInfo\\\":[{\\\"Timestamp\\\":\\\"0\\\",\\\"Status\\\":\\\"1\\\"}],\\\"DeviceModel\\\":\\\"ANE-LX1\\\",\\\"MultitaskingSupported\\\":true,\\\"DeviceName\\\":\\\"marky\\\",\\\"DeviceSystemName\\\":\\\"Android\\\",\\\"DeviceSystemVersion\\\":\\\"28\\\",\\\"Languages\\\":\\\"ru\\\",\\\"WiFiMacAddress\\\":\\\"02:00:00:00:00:00\\\",\\\"WiFiNetworksData\\\":{\\\"BBSID\\\":\\\"02:00:00:00:00:00\\\",\\\"SignalStrength\\\":\\\"-47\\\",\\\"Channel\\\":\\\"null\\\"},\\\"CellTowerId\\\":\\\"-1\\\",\\\"LocationAreaCode\\\":\\\"-1\\\",\\\"ScreenSize\\\":\\\"1080x2060\\\",\\\"RSA_ApplicationKey\\\":\\\"2C501591EA5BF79F1C0ABA8B628C2571\\\",\\\"MCC\\\":\\\"286\\\",\\\"MNC\\\":\\\"02\\\",\\\"OS_ID\\\":\\\"1f32651b72df5515\\\",\\\"SDK_VERSION\\\":\\\"3.10.0\\\",\\\"Compromised\\\":0,\\\"Emulator\\\":0}");
+        createPinRequestBody.add("mobileSDKKAV", "{\\\"osVersion\\\":0,\\\"KavSdkId\\\":\\\"\\\",\\\"KavSdkVersion\\\":\\\"\\\",\\\"KavSdkVirusDBVersion\\\":\\\"SdkVirusDbInfo(year=0, month=0, day=0, hour=0, minute=0, second=0, knownThreatsCount=0, records=0, size=0)\\\",\\\"KavSdkVirusDBStatus\\\":\\\"\\\",\\\"KavSdkVirusDBStatusDate\\\":\\\"\\\",\\\"KavSdkRoot\\\":false,\\\"LowPasswordQuality\\\":false,\\\"NonMarketAppsAllowed\\\":false,\\\"UsbDebugOn\\\":false,\\\"ScanStatus\\\":\\\"NONE\\\"}");
 
-            HttpEntity<MultiValueMap<String, String>> createPinRequest = new HttpEntity<MultiValueMap<String, String>>(createPinRequestBody, headers);
+        HttpEntity<MultiValueMap<String, String>> createPinRequest = new HttpEntity<MultiValueMap<String, String>>(createPinRequestBody, headers);
 
-            ResponseEntity<String> createPinResponse = restTemplate().postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", createPinRequest , String.class);
+        ResponseEntity<String> createPinResponse = restTemplate.postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", createPinRequest , String.class);
 
-            Integer responseCode = SberUtils.getCodeFromResponse(createPinResponse.getBody());
+        Integer responseCode = SberUtils.getCodeFromResponse(createPinResponse.getBody());
 
-            if(responseCode == null || responseCode != 0) {
-                throw new ServiceException("Invalid response code from SBER", HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-
-            String responseToken = SberUtils.getTokenFromResponse(createPinResponse.getBody());
-            String responseHost = SberUtils.getHostFromResponse(createPinResponse.getBody());
-
-            if(responseToken == null)
-                throw new ServiceException("SBER Token not found", HttpStatus.INTERNAL_SERVER_ERROR);
-
-            if(responseHost == null)
-                throw new ServiceException("SBER Host not found", HttpStatus.INTERNAL_SERVER_ERROR);
-
-            token = responseToken;
-            host = responseHost;
-
-            state = SberRegisterState.FINISHED;
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(responseCode == null || responseCode != 0) {
+            throw new ServiceException("Invalid response code from SBER", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        String responseToken = SberUtils.getTokenFromResponse(createPinResponse.getBody());
+        String responseHost = SberUtils.getHostFromResponse(createPinResponse.getBody());
+
+        if(responseToken == null)
+            throw new ServiceException("SBER Token not found", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        if(responseHost == null)
+            throw new ServiceException("SBER Host not found", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        token = responseToken;
+        host = responseHost;
+
+        state = SberRegisterState.FINISHED;
     }
 
     public UUID getUserId() {
